3af12e218d34bc69638d87a2f2a6dc71ae8396e7
EC2 Default User <ec2-user@ip-172-31-21-70.eu-west-2.compute.internal>
2020-01-12 13:11:48 +0000

    error handling added to postSafePlace

--- postSafePlaceById/lambda_function.py
+++ postSafePlaceById/lambda_function.py
@@ -5,21 +5,40 @@ dynamodb = boto3.resource("dynamodb")
 
 def lambda_handler(event, context):
     payload = json.loads(event["body"])
-    newItem={
-            'author': payload["author"],
-            'place_id': event["pathParameters"]["place_id"],
-            'rating': "0",
-            'formatted_address': payload["formatted_address"],
-            'latitude': payload['latitude'],
-            'longitude': payload['longitude'],
-            'place_name': payload['place_name'],
-            'weekday_text': payload['weekday_text']
-        }
-    table = dynamodb.Table("placesTable")
-    response = table.put_item(
-        Item = newItem
-        )
+    
+    requiredParams = [
+        'author', 'formatted_address', 'latitude', 'longitude', 'place_name', 'weekday_text'
+        ]
+    
+    missingParams = False
+    
+    for i in requiredParams:
+        if payload.get(i) == None:
+            missingParams = True
+    
+    if missingParams == True:
+        statusCode = 400
+        body = json.dumps("Missing request parameters")
+    else:
+        newItem={
+                'author': payload["author"],
+                'place_id': event["pathParameters"]["place_id"],
+                'rating': "0",
+                'formatted_address': payload["formatted_address"],
+                'latitude': payload['latitude'],
+                'longitude': payload['longitude'],
+                'place_name': payload['place_name'],
+                'weekday_text': payload['weekday_text']
+            }
+        table = dynamodb.Table("placesTable")
+        response = table.put_item(
+            Item = newItem
+            )
+            
+        statusCode=200
+        body = json.dumps(newItem)
+    
     return {
-        'statusCode': 200,
-        'body': json.dumps(newItem)
+        'statusCode': statusCode,
+        'body': body
     }
