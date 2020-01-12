fe3272449a1ef1c2688617ae7124032cc29a6a6a
EC2 Default User <ec2-user@ip-172-31-21-70.eu-west-2.compute.internal>
2020-01-12 13:18:48 +0000

    error handling added to getAllSafePlaces

--- getAllSafePlaces/lambda_function.py
+++ getAllSafePlaces/lambda_function.py
@@ -14,9 +14,21 @@ class DecimalEncoder(json.JSONEncoder):
 dynamodb = boto3.resource("dynamodb")
 
 def lambda_handler(event, context):
-    table = dynamodb.Table("safeplaceTable")
-    response = table.scan()
+    try:
+        table = dynamodb.Table("safeplaceTable")
+        response = table.scan()
+        statusCode = 200
+    except:
+        statusCode = 500
+
+    if statusCode == 200:
+        body = json.dumps(response, cls=DecimalEncoder)
+    elif statusCode == 500:
+        body = "Internal Server Error"
+    else:
+        body = "Unknown Error"
+    
     return {
-        'statusCode': 200,
-        'body': json.dumps(response, cls=DecimalEncoder)
+        'statusCode': statusCode,
+        'body': body
     }
