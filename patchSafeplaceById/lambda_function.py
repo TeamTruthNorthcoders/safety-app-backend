6bb6d634ac7972f0f4e2a5c3610064c5c31679b4
EC2 Default User <ec2-user@ip-172-31-21-70.eu-west-2.compute.internal>
2020-01-13 12:01:07 +0000

    modified patchSafeplace to display negative numbers as floats in its response

--- patchSafeplaceById/lambda_function.py
+++ patchSafeplaceById/lambda_function.py
@@ -5,7 +5,7 @@ import decimal
 class DecimalEncoder(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, decimal.Decimal):
-            if o % 1 > 0:
+            if o % 1 > 0 or o % 1 < 0:
                 return float(o)
             else:
                 return int(o)
@@ -17,19 +17,30 @@ dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('safeplaceTable')
 
 def lambda_handler(event, context):
-    inc_rating = int(json.dumps(json.loads(event["body"])["inc_votes"]))
+    inc_rating = int(json.dumps(json.loads(event["body"])["inc_rating"]))
     
     response = table.update_item(
         Key={
         "place_id": event["pathParameters"]["place_id"]
         },
-    UpdateExpression="SET rating = :new_rating",
+    UpdateExpression="SET rating = :new_rating + rating",
     ExpressionAttributeValues={
         ':new_rating': inc_rating
     },
     ReturnValues="ALL_NEW"
     )
+    
+    patchedItem = {
+         "author": response["Attributes"]["formatted_address"],
+          "formatted_address": response["Attributes"]["formatted_address"],
+          "place_name": response["Attributes"]["place_name"],
+          "place_id": response["Attributes"]["place_id"],
+          "rating": response["Attributes"]["rating"],
+          "longitude": decimal.Decimal(str(response["Attributes"]["longitude"])),
+          "latitude": decimal.Decimal(str(response["Attributes"]["latitude"])),
+    }
+    
     return {
         "statusCode": 200,
-        "body": json.dumps(response["Attributes"], indent=4, cls=DecimalEncoder)
+        "body": json.dumps(patchedItem, cls=DecimalEncoder)
     }
