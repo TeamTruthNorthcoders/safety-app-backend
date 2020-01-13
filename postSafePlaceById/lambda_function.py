5abb2e7e88cda0b76fe94779b53b06cef861feba
EC2 Default User <ec2-user@ip-172-31-21-70.eu-west-2.compute.internal>
2020-01-13 11:04:05 +0000

    postSafePlaceById modified to accept numbers for lolongitude and latitude

--- postSafePlaceById/lambda_function.py
+++ postSafePlaceById/lambda_function.py
@@ -1,9 +1,20 @@
 import json
 import boto3
+import decimal
 
 dynamodb = boto3.resource("dynamodb")
 
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
+
 def lambda_handler(event, context):
+    print(event)
     payload = json.loads(event["body"])
     
     requiredParams = [
@@ -23,20 +34,21 @@ def lambda_handler(event, context):
         newItem={
                 'author': payload["author"],
                 'place_id': event["pathParameters"]["place_id"],
-                'rating': "0",
+                'rating': 0,
                 'formatted_address': payload["formatted_address"],
-                'latitude': payload['latitude'],
-                'longitude': payload['longitude'],
+                'latitude': decimal.Decimal(str(payload['latitude'])),
+                'longitude': decimal.Decimal(str(payload['longitude'])),
                 'place_name': payload['place_name'],
                 'weekday_text': payload['weekday_text']
             }
-        table = dynamodb.Table("placesTable")
+
+        table = dynamodb.Table("safeplaceTable")
         response = table.put_item(
             Item = newItem
             )
-            
+        
         statusCode=200
-        body = json.dumps(newItem)
+        body = json.dumps(newItem, cls=DecimalEncoder)
     
     return {
         'statusCode': statusCode,
