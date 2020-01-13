e68f888071bd9c7b7b6effdcbc9b0d567a4da192
David Elertas <eldeividas@gmail.com>
2020-01-13 13:13:43 +0000

    Refactored get reviews by place id,added error handling

--- getReviewsByPlaceId/lambda_function.py
+++ getReviewsByPlaceId/lambda_function.py
@@ -9,33 +9,27 @@ dynamodb = boto3.resource("dynamodb")
 class DecimalEncoder(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, decimal.Decimal):
-            if o % 1 > 0:
+            if o % 1 > 0 or o % 1 < 0:
                 return float(o)
             else:
                 return int(o)
         return super(DecimalEncoder, self).default(o)
         
-        
+     
 def lambda_handler(event, context):
-    table = dynamodb.Table("placeReviews")
-    print(event)
+    table = dynamodb.Table("placeReviewsTable")
     place_id = event["pathParameters"]["place_id"]
     fe = Key('place_id').eq(place_id)
-    pe = "#a,#r,#b,#pi, #ri"
+    pe = "#author, #rating,#review,#place_id,#review_id"
     # Expression Attribute Names for Projection Expression only.
-    ean = { "#a": "author", "#r": "rating", "#b": "body","#pi":"place_id","#ri":"review_id"}
+    ean = {"#author": "author", "#rating": "rating", "#review": "review","#place_id":"place_id","#review_id":"review_id"}
     esk = None
-    
 
     response = table.scan(
     FilterExpression=fe,
     ProjectionExpression=pe,
     ExpressionAttributeNames=ean
     )
-
-    for i in response['Items']:
-     print(json.dumps(i, cls=DecimalEncoder))
-
     while 'LastEvaluatedKey' in response:
         response = table.scan(
         ProjectionExpression=pe,
@@ -43,15 +37,16 @@ def lambda_handler(event, context):
         ExpressionAttributeNames= ean,
         ExclusiveStartKey=response['LastEvaluatedKey']
         )
-
-    for i in response['Items']:
-        print(json.dumps(i, cls=DecimalEncoder))
-        
-    return {
-        'statusCode': 200,
-        'body': json.dumps(response)
-    }
-
+    if len(response["Items"]) == 0:
+        return {
+            'statusCode': 404,
+            'body': "no reviews found for the place"
+        }
+    else:
+        return {
+            'statusCode': 200,
+            'body': json.dumps(response, cls=DecimalEncoder)
+        }
 
 
 
