e5a48e1bacee9dad01858684270d58d8b1b1edcf
EC2 Default User <ec2-user@ip-172-31-21-70.eu-west-2.compute.internal>
2020-01-13 12:07:20 +0000

    patchSafeplace error handled

--- patchSafeplaceById/lambda_function.py
+++ patchSafeplaceById/lambda_function.py
@@ -17,30 +17,37 @@ dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('safeplaceTable')
 
 def lambda_handler(event, context):
-    inc_rating = int(json.dumps(json.loads(event["body"])["inc_rating"]))
     
-    response = table.update_item(
-        Key={
-        "place_id": event["pathParameters"]["place_id"]
+    if "inc_rating" not in json.loads(event["body"]):
+        statusCode = 400
+        body = "Missing request parameter"
+    else:
+        inc_rating = int(json.dumps(json.loads(event["body"])["inc_rating"]))
+        
+        response = table.update_item(
+            Key={
+            "place_id": event["pathParameters"]["place_id"]
+            },
+        UpdateExpression="SET rating = :new_rating + rating",
+        ExpressionAttributeValues={
+            ':new_rating': inc_rating
         },
-    UpdateExpression="SET rating = :new_rating + rating",
-    ExpressionAttributeValues={
-        ':new_rating': inc_rating
-    },
-    ReturnValues="ALL_NEW"
-    )
-    
-    patchedItem = {
-         "author": response["Attributes"]["formatted_address"],
-          "formatted_address": response["Attributes"]["formatted_address"],
-          "place_name": response["Attributes"]["place_name"],
-          "place_id": response["Attributes"]["place_id"],
-          "rating": response["Attributes"]["rating"],
-          "longitude": decimal.Decimal(str(response["Attributes"]["longitude"])),
-          "latitude": decimal.Decimal(str(response["Attributes"]["latitude"])),
-    }
-    
+        ReturnValues="ALL_NEW"
+        )
+        
+        patchedItem = {
+             "author": response["Attributes"]["formatted_address"],
+              "formatted_address": response["Attributes"]["formatted_address"],
+              "place_name": response["Attributes"]["place_name"],
+              "place_id": response["Attributes"]["place_id"],
+              "rating": response["Attributes"]["rating"],
+              "longitude": decimal.Decimal(str(response["Attributes"]["longitude"])),
+              "latitude": decimal.Decimal(str(response["Attributes"]["latitude"])),
+        }
+        statusCode = 200
+        body = json.dumps(patchedItem, cls=DecimalEncoder)
+        
     return {
-        "statusCode": 200,
-        "body": json.dumps(patchedItem, cls=DecimalEncoder)
+        "statusCode": statusCode,
+        "body": body
     }
